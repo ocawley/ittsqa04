93476bdc8cd42fd69dce95d6e8385c23476b4f30
x00114517 <x00114517@gmail.com>
2014-10-28 11:57:38 +0000

    here is the blackbox shell file(checking for bugs)

---
+++ x00114517.sh
@@ -0,0 +1,39 @@
+#Test Case 1  
+	#Input: 30 40
+	#Expected output: Fail
+       	 #observed output : Component Fail.
+	java Grades 30 40
+
+	#Test Case 2 
+	#Input: 30 60
+	#Expected output: Component Fail
+	#observed output : Component Fail
+	java Grades 30 60
+
+	#Test Case 3
+	#Input: 60 30
+	#Expected output: Component Fail
+	#observed output : Component Fail
+	java Grades 60 30
+
+
+	#Test Case 4
+	#Input: 50 80
+	#Expected output: Pass
+	#observed output : Pass
+	java Grades 50 80
+
+	#Test Case 5 
+	#Input: 80 100
+	#Expected output: Pass with distinction
+	#observed output: Pass with distinction
+	java Grades 80 100
+
+	#Test Case 5 
+	#Input: 120 300
+	#Expected output: Invalid Input
+	#observed output: Invalid Input
+	java Grades 120 300
+
+
+
